37b11e80e8474a6182cb4517de19b11e51c816fc
BugrahanTelli <mbugrahan.telli@gmail.com>
2024-10-05 18:16:58 +0300

    EJS Done

--- app.js
+++ app.js
@@ -1,15 +1,27 @@
 const express = require("express")
+const ejs = require("ejs")
+const path = require("path")
 
 const app = express()
 
+app.set("view engine", "ejs")
+
+app.use(express.static("public"))
+
 app.get("/", (req, res) => {
-    const blog = {
-        id: 1,
-        title: "Clean_Blog",
-        description: "Clean Blog Project"
-    }
-    res.send(blog)
+    res.render("index")
 })
+app.get("/about", (req, res) => {
+    res.render("about")
+})
+app.get("/add", (req, res) => {
+    res.render("add")
+})
+app.get("/post", (req, res) => {
+    res.render("post")
+})
+
+
 
 const port = 3000
 
